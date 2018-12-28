678601c5725ca9e28da4fe66052ac6d6786c5696
Jonathan Owens <jo@jondionowens.com>
2018-12-27 20:16:21 -0800

    Cleaning up directory for pull

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use(express.static(path.join(__dirname, './public')));
+app.use('/:id', express.static(path.join(__dirname, './public')));
+
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`Server running at port ${port}`);
 });
+
+module.exports = app;
